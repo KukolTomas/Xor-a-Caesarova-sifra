fc9e5729b1b5a15dcbf5dcbb7be74d192b389bd0
Thomas Kukol <tomas.konicek1@gmail.com>
2021-03-18 19:44:00 +0100

    Přidejte soubory projektu.

---
+++ Sifrovani/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sifrovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Pokudhledatepomocnourukunajdetejinakoncipaze";
+            string key = "PRAVDA";
+            char[] charArr = new char[text.Length];
+            char[] decharArr = new char[text.Length];
+
+            string rndText = "Nahodnytext";
+            char[] originalTextArray = new char[rndText.Length];
+            char[] decodedOriginalTextArray = new char[rndText.Length];
+            int move = 7;
+
+            //Xor zasifrovani
+            for (int i = 0; i < text.Length; i++)
+            {
+                charArr[i] = (char)(text[i] ^ key[i % key.Length]);
+            }
+            //Xor desifrovani
+            for (int i = 0; i < text.Length; i++)
+            {
+                decharArr[i] = (char)(charArr[i] ^ key[i % key.Length]);
+            }
+            // Cesarova sifra - zasifrovani
+            for (int i = 0; i < rndText.Length; i++)
+            {
+                originalTextArray[i] = rndText[i];
+            }
+            for (int i = 0; i < rndText.Length; i++)
+            {
+                int reset = 0;
+                reset = rndText[i] + move;
+                if(reset > 122)
+                {
+                    reset -= 26;
+                }
+                if(reset < 97)
+                {
+                    reset += 26;
+                }
+                originalTextArray[i] = (char)(reset);
+            }
+            // Caesarova sifra - odsifrovani
+            for (int i = 0; i < rndText.Length; i++)
+            {
+                int reset = 0;
+                reset = originalTextArray[i] - move;
+                if(reset < 97)
+                {
+                    reset += 26;
+                }
+                decodedOriginalTextArray[i] = (char)(reset);
+
+            }
+            Console.WriteLine(charArr);
+            Console.WriteLine(decharArr);
+            Console.WriteLine(originalTextArray);
+            Console.WriteLine(decodedOriginalTextArray);
+            Console.ReadLine();
+        }
+    }
+}
